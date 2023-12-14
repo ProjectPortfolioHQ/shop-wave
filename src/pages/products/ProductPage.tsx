e4e859e9fd4bf1c62cce1b572c49b25044e46900
Cuc Trinh <huucucdev@gmail.com>
2023-12-15 06:47:40 +0700

    add react hook form and layout product

--- src/pages/products/ProductPage.tsx
+++ src/pages/products/ProductPage.tsx
@@ -1,39 +1,40 @@
+import FilterProduct from "./components/FilterProduct";
 import ProductItem from "./components/ProductItem";
 
 const products = [
   {
     id: 1,
-    image: "posts/image1.webp",
+    image: "posts/phone.png",
     title: "Product 1",
     price: "$19.99",
   },
   {
     id: 2,
-    image: "posts/image1.webp",
+    image: "posts/phone.png",
     title: "Product 2",
     price: "$29.99",
   },
   {
     id: 3,
-    image: "posts/image1.webp",
+    image: "posts/phone.png",
     title: "Product 2",
     price: "$29.99",
   },
   {
     id: 4,
-    image: "posts/image1.webp",
+    image: "posts/phone.png",
     title: "Product 2",
     price: "$29.99",
   },
   {
     id: 5,
-    image: "posts/image1.webp",
+    image: "posts/phone.png",
     title: "Product 2",
     price: "$29.99",
   },
   {
     id: 6,
-    image: "posts/image1.webp",
+    image: "posts/phone.png",
     title: "Product 2",
     price: "$29.99",
   },
@@ -41,15 +42,23 @@ const products = [
 
 const ProductPage = () => {
   return (
-    <div className="flex flex-wrap justify-center">
-      {products.map((product) => (
-        <ProductItem
-          key={product.id}
-          image={product.image}
-          title={product.title}
-          price={product.price}
-        />
-      ))}
+    <div className="container grid grid-cols-12 gap-4">
+      <div className="col-span-2">
+        <FilterProduct />
+      </div>
+
+      <div className="col-span-10">
+        <div className="grid grid-cols-5 gap-4">
+          {products.map((product) => (
+            <ProductItem
+              key={product.id}
+              image={product.image}
+              title={product.title}
+              price={product.price}
+            />
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
